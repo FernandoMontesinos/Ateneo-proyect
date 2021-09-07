9471d60fa92b993aec8240491e0e5a1311f8a0d8
FernandoMontesinos <fmontesinosestrada@gmail.com>
2021-09-07 00:47:23 -0500

    070921

---
+++ js/chart.js
@@ -0,0 +1,57 @@
+const testRealizados = document.getElementById("testChart");
+
+const policiaN = {
+    label: "Policia Nacional",
+    lineTension: 0,
+    fill: false,
+    borderColor: 'rgba(240, 60, 72, 1)',
+    backgroundColor: 'rgba(240, 60, 72, 1)'
+};
+
+const GuardiaC = {
+    label: "Guardia Civil",
+    data: [0, 70],
+    lineTension: 0,
+    fill: false,
+    borderColor: 'rgb(255, 223, 0)',
+    backgroundColor: 'rgb(255, 223, 0)'
+};
+
+const data = {
+    labels: ["10", "20"],
+    datasets: [policiaN, GuardiaC]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {
+        plugins: {
+            legend: {
+                position: 'bottom',
+                align: 'end',
+                labels: {
+                    boxWidth: 18
+                }
+            }
+        },
+        scales: {
+            x: {
+                title:{
+                    display: true,
+                    text: 'Días',
+                    color: 'rgb(255, 223, 0)'
+                }
+            },
+            y: {
+                title:{
+                    display: true,
+                    text: 'Tests',
+                    color: 'rgb(255, 223, 0)'
+                }
+            }
+        }  
+    }
+};
+
+const lineChart = new Chart(testRealizados, config);
