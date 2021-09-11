db66deaaed9e76c02c0fe2d868e411bc3e7dc971
FernandoMontesinos <fmontesinosestrada@gmail.com>
2021-09-11 01:01:05 -0500

    110921

---
+++ js/chartCurso.js
@@ -0,0 +1,44 @@
+const testRealizados = document.getElementById("testChart");
+
+
+const GuardiaC = {
+    label: "Guardia Civil",
+    data: [0, 70],
+    lineTension: 0,
+    fill: false,
+    borderColor: 'rgb(255, 223, 0)',
+    backgroundColor: 'rgb(255, 223, 0)'
+};
+
+const data = {
+    labels: ["10", "20"],
+    datasets: [GuardiaC]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {
+        plugins: {
+            legend: false,
+        },
+        scales: {
+            x: {
+                title:{
+                    display: true,
+                    text: 'Días',
+                    color: 'rgb(255, 223, 0)'
+                }
+            },
+            y: {
+                title:{
+                    display: true,
+                    text: 'Tests',
+                    color: 'rgb(255, 223, 0)'
+                }
+            }
+        }  
+    }
+};
+
+const lineChart = new Chart(testRealizados, config);
